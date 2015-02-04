8e4c4257b15e08b911c3fda0a94198733fe2165e
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-02-03 16:45:38 -0800

    Added esri-leaflet

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,41 +1,36 @@
 ﻿/*global L*/
 (function (L) {
 	"use strict";
-	var map, imgSvcUrl;
+	var map, imgSvcUrl, baseLayer, imgLayer;
 
 	imgSvcUrl = "http://hqolymgis99t/arcgis/rest/services/Airport/AirportRasterTest/ImageServer";
 
 	map = L.map('map').setView(
-		 [46.97116044960811, -123.93646717071533], 15);
+		 [47.41322033015946, -120.80566406246835], 7);
 
-	L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	baseLayer = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: 'Map data &copy; <a href="//openstreetmap.org">OpenStreetMap</a> contributors, <a href="//creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 		maxZoom: 18
 	}).addTo(map);
 
+	imgLayer = L.esri.Layers.imageMapLayer(imgSvcUrl, {
+		opacity: 0.5
+	}).addTo(map);
+
+	L.control.layers({ OpenStreetMap: baseLayer }, {"Raster": imgLayer}).addTo(map);
+
 	map.on('click', function (e) {
-		var x, y;
-		x = e.latlng.lng;
-		y = e.latlng.lat;
-		console.debug([x, y]);
-
-		var request = new XMLHttpRequest();
-
-
-		var point = {
-			x: x,
-			y: y,
-			spatialReference: {
-				wkid: 4326
+		L.esri.Tasks.identifyImage({
+			url: imgSvcUrl
+		}).at([e.latlng.lat, e.latlng.lng]).run(function (error, identifyImageResponse, rawResponse) {
+			if (error) {
+				console.error(error);
+			} else {
+				console.debug({
+					identifyImageRespnose: identifyImageResponse,
+					rawResponse: rawResponse
+				});
 			}
-		};
-		request.open("GET", imgSvcUrl + "/identify?f=json&geometryType=esriGeometryPoint&outSR=4326&geometry=" + JSON.stringify(point));
-
-		request.onloadend = function () {
-			var json = JSON.parse(this.responseText);
-			console.debug(json);
-		};
-
-		request.send();
+		});
 	});
 }(L));
