9a424d5068022054c0f2f093c07ce77a53d23e0f
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-02-03 17:26:58 -0800

    Added layers from ThunderForest :leaves:

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,22 +1,39 @@
 ﻿/*global L*/
 (function (L) {
 	"use strict";
-	var map, imgSvcUrl, osmLayer, imgLayer, landscapeLayer;
+	var map, imgSvcUrl, osmLayer, imgLayer, landscapeLayer, openCycleMapLayer, transportLayer, outdoorsLayer, transportDarkLayer;
+
+	/**
+	 * Creates a TileLayer using tiles from thunderforest.com.
+	 * @param {string} name - The name of the layer. This is part of the URL.
+	 */
+	function createThunderforestTileLayer(name) {
+		return L.tileLayer('http://{s}.tile.thunderforest.com/' + name + '/{z}/{x}/{y}.png', {
+			attribution: 'Maps © <a href="http://www.thunderforest.com/">Thunderforest</a>, Data © <a href="//openstreetmap.org">OpenStreetMap</a> contributors',
+			subdomains: ['a', 'b', 'c'],
+		});
+	}
 
 	imgSvcUrl = "http://hqolymgis99t/arcgis/rest/services/Airport/AirportRasterTest/ImageServer";
 
-	map = L.map('map').setView(
-		 [47.41322033015946, -120.80566406246835], 7);
+	map = L.map('map', {
+		center: [47.41322033015946, -120.80566406246835],
+		zoom: 7,
+		maxZoom: 18
+	});
+
+	// Create basemaps
 
 	osmLayer = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: 'Map data &copy; <a href="//openstreetmap.org">OpenStreetMap</a> contributors, <a href="//creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		maxZoom: 18
 	}).addTo(map);
+	landscapeLayer = createThunderforestTileLayer("landscape");
+	openCycleMapLayer = createThunderforestTileLayer("cycle");
+	transportLayer = createThunderforestTileLayer("transport");
+	outdoorsLayer = createThunderforestTileLayer("outdoors");
+	transportDarkLayer = createThunderforestTileLayer("outdoors-dark");
 
-	landscapeLayer = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
-		attribution: 'Maps © <a href="http://www.thunderforest.com/">Thunderforest</a>, Data © <a href="//openstreetmap.org">OpenStreetMap</a> contributors',
-		maxZoom: 18
-	});
+	// Create overlays
 
 	imgLayer = L.esri.Layers.imageMapLayer(imgSvcUrl, {
 		opacity: 0.5
@@ -24,7 +41,11 @@
 
 	L.control.layers({
 		OpenStreetMap: osmLayer,
-		"Landscape": landscapeLayer
+		"Landscape": landscapeLayer,
+		"Transport": transportLayer,
+		"Transport Dark": transportDarkLayer,
+		"OpenCycleMap": openCycleMapLayer,
+		"Outdoors": outdoorsLayer
 	}, {
 		"Airport Surfaces": imgLayer
 	}).addTo(map);
