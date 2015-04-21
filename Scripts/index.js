1e7ba564d1c12769376bb7251ead287ebe1fe581
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-04-21 14:40:08 -0700

    Updated test image service URL

--- Scripts/index.js
+++ Scripts/index.js
@@ -127,7 +127,7 @@
 		}).addTo(map).bindPopup(createDl(surfacePenetrationInfo));
 	}
 
-	imgSvcUrl = "http://hqolymgis99t/arcgis/rest/services/Airport/AirportRasterTest/ImageServer";
+	imgSvcUrl = "http://hqolymgis99t.wsdot.loc/arcgis/rest/services/Airport/Test_obstruction_raster_int/ImageServer";
 
 	map = L.map('map', {
 		center: [47.41322033015946, -120.80566406246835],
